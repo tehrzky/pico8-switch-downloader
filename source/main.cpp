0283c2c4d09bf67e44789ed685c0fa8ecf605d26
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-08 16:45:27 +0800

    Create main.cpp

---
+++ source/main.cpp
@@ -0,0 +1,43 @@
+#include <switch.h>
+#include <stdio.h>
+#include "config.hpp"
+#include "downloader.hpp"
+
+int main(int argc, char **argv) {
+    consoleInit(NULL);
+    socketInitializeDefault(); // Required for network stack on Switch
+
+    load_config();
+
+    printf("\x1b[1;1HPICO-8 Downloader & Browser");
+    printf("\x1b[3;1HCurrent Path: %s", g_config.download_path.c_str());
+    printf("\x1b[5;1HPress A to Download Demo Cartridge (Celeste)");
+    printf("\x1b[6;1HPress + to Exit");
+
+    while (appletMainLoop()) {
+        padUpdate(&pad);
+        u64 kDown = padGetButtonsDown(&pad);
+
+        if (kDown & HidNpadButton_Plus) break;
+
+        if (kDown & HidNpadButton_A) {
+            printf("\x1b[8;1HDownloading cartridge...");
+            consoleUpdate(NULL);
+            
+            std::string dest = g_config.download_path + "celeste.p8.png";
+            bool success = download_file("https://www.lexaloffle.com/bbs/cdata/0/celeste.p8.png", dest);
+
+            if (success) {
+                printf("\x1b[9;1HDownload successful!");
+            } else {
+                printf("\x1b[9;1HDownload failed. Verify network connection.");
+            }
+        }
+
+        consoleUpdate(NULL);
+    }
+
+    socketExit();
+    consoleExit(NULL);
+    return 0;
+}
